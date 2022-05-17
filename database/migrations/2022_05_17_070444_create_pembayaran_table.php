e73424ed66cb56b5f7c144f7f417bb8daaecff07
unknown <yusufprataman@gmail.com>
2022-05-17 14:07:44 +0700

    erd-yusuf

---
+++ database/migrations/2022_05_17_070444_create_pembayaran_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pembeli_id')->constrained('pembeli');
+            $table->foreignId('baju_id')->constrained('baju');
+            $table->foreignId('admin_id')->constrained('admin');
+            $table->integer('jumlah')->default(0);
+            $table->integer('total_harga')->default(0);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+};
